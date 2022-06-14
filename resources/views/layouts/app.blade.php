804251a9ec7e560fecdea004930f4b96fa7d4529
Hersarma <hersarma@gmail.com>
2022-06-14 06:34:46 +0200

    nav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full bg-gray-100">
     <head>
         <meta charset="utf-8">
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
--- resources/views/menu_parts/admin/navbar.blade.php
+++ resources/views/menu_parts/admin/navbar.blade.php
@@ -34,7 +34,7 @@
           <div x-data="{ open: false }" class="ml-3 relative">
             <div>
               <button @click="open =! open" type="button" class="max-w-xs rounded-full flex items-center text-sm lg:p-2" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                <span class="hidden ml-1 text-white text-sm font-medium lg:block"><span class="sr-only">Open user menu for </span>{{ auth()->user()->name }}</span>
+                <span class="ml-1 text-white text-sm font-medium"><span class="sr-only">Open user menu for </span>{{ auth()->user()->name }}</span>
                 <!-- Heroicon name: solid/chevron-down -->
                 <svg class="hidden flex-shrink-0 ml-1 h-5 w-5 text-yellow-400 lg:block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                   <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
