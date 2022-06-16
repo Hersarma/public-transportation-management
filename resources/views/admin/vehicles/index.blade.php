8b0f25cc48a352b782242f1c9ba05fb069f9b4f0
Hersarma <hersarma@gmail.com>
2022-06-16 21:02:11 +0200

    create vehicle

--- resources/views/admin/vehicles/index.blade.php
+++ resources/views/admin/vehicles/index.blade.php
@@ -21,9 +21,9 @@
       <h1 class="text-xl font-semibold text-gray-900">Vozni park</h1>
       <p class="mt-2 text-sm text-gray-700">Lista svih vozila.</p>
     </div>
-    <div x-data="{open: false}" class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-      <button x-on:click="open = ! open" type="button" class="inline-flex items-center justify-center rounded-md border border-transparent bg-gray-700 px-4 py-2 text-sm font-medium text-yellow-400 shadow-sm hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-900 focus:ring-offset-2 sm:w-auto">Dodaj vozilo</button>
-      @include('admin.vehicles.modals.create')
+    <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+      <button type="button" class="inline-flex items-center justify-center rounded-md border border-transparent bg-gray-700 px-4 py-2 text-sm font-medium text-yellow-400 shadow-sm hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-900 focus:ring-offset-2 sm:w-auto">Dodaj vozilo</button>
+      
     </div>
   </div>
   <div class="-mx-4 mt-8 overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:-mx-6 md:mx-0 md:rounded-lg">
